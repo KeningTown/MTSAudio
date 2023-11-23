79c8f997386eb447990fb437af7811e255e4df2a
dfoxs353 <platoleg37@gmail.com>
2023-11-24 00:07:08 +0300

    add provides to retrofit, okhttpClient, LocalUserRepository, AuthRepository

--- Mobile/app/src/main/java/com/example/mts_audio/di/DataModule.kt
+++ Mobile/app/src/main/java/com/example/mts_audio/di/DataModule.kt
@@ -1,11 +1,95 @@
 package com.example.mts_audio.di
 
+import android.content.Context
+import android.content.SharedPreferences
+import com.example.mts_audio.common.AuthAuthenticator
+import com.example.mts_audio.common.AuthInterceptor
+import com.example.mts_audio.data.remote.auth.AuthApi
+import com.example.mts_audio.data.repository.AuthRepository
+import com.example.mts_audio.data.repository.LocalUserRepository
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.Dispatchers
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class DataModule {
-    //TODO provide data
+
+    @Provides
+    @Singleton
+    fun provideLocalUserRepository(sharedPreferences: SharedPreferences): LocalUserRepository {
+        return LocalUserRepository(sharedPreferences)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSharedPreference(@ApplicationContext context: Context): SharedPreferences {
+        val PREFS_NAME = "user_prefs"
+        return context.getSharedPreferences(context.packageName + PREFS_NAME, Context.MODE_PRIVATE)
+    }
+
+    @Singleton
+    @Provides
+    fun provideAuthInterceptor(tokenManager: LocalUserRepository): AuthInterceptor =
+        AuthInterceptor(tokenManager)
+
+    @Singleton
+    @Provides
+    fun provideAuthAuthenticator(tokenManager: LocalUserRepository): AuthAuthenticator =
+        AuthAuthenticator(tokenManager)
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        authInterceptor: AuthInterceptor,
+        authAuthenticator: AuthAuthenticator,
+    ): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        return OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+            .addInterceptor(loggingInterceptor)
+            .authenticator(authAuthenticator)
+            .build()
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideRetrofitBuilder(): Retrofit.Builder =
+        Retrofit.Builder()
+            .baseUrl("https://mooduck-service-api.onrender.com/api/")
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .addConverterFactory(GsonConverterFactory.create())
+
+    @Singleton
+    @Provides
+    fun provideAuthAPIService(retrofit: Retrofit.Builder): AuthApi =
+        retrofit
+            .client(
+                OkHttpClient.Builder()
+                    .connectTimeout(10, TimeUnit.SECONDS)
+                    .readTimeout(10, TimeUnit.SECONDS)
+                    .writeTimeout(10, TimeUnit.SECONDS)
+                    .build()
+            )
+            .build()
+            .create(AuthApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideRemoteAuthRepository(authApi: AuthApi): AuthRepository {
+        return AuthRepository(authApi, Dispatchers.IO)
+    }
 }
