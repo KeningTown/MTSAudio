c865b8424db797f954276e1bd677e83304cd6d68
dfoxs353 <platoleg37@gmail.com>
2023-11-23 23:40:27 +0300

    add DataModule

--- Mobile/app/src/main/java/com/example/mts_audio/di/DataModule.kt
+++ Mobile/app/src/main/java/com/example/mts_audio/di/DataModule.kt
@@ -1,4 +1,11 @@
 package com.example.mts_audio.di
 
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
 class DataModule {
+    //TODO provide data
 }
