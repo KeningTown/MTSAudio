19769f41f614cccfac3935c7e383005f07942aed
dfoxs353 <platoleg37@gmail.com>
2023-11-23 23:46:55 +0300

    add AuthAuthenticator and AuthInterceptor

---
+++ Mobile/app/src/main/java/com/example/mts_audio/common/AuthAuthenticator.kt
@@ -0,0 +1,68 @@
+package com.example.mts_audio.common
+
+import android.util.Log
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import okhttp3.Authenticator
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.Route
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
+import javax.inject.Inject
+
+class AuthAuthenticator @Inject constructor(
+    private val userRepository: LocalUserRepository
+) : Authenticator {
+
+
+    override fun authenticate(route: Route?, response: Response): Request? {
+        return runBlocking {
+            val token = userRepository.getRefreshToken()
+            when (val newToken = getNewToken(token)) {
+                is Result.Success -> {
+                    newToken.data.apply {
+                        userRepository.saveJWToken(refreshToken, accessToken)
+                    }
+                    response.request.newBuilder()
+                        .header("Authorization", "Bearer ${newToken.data.accessToken}")
+                        .build()
+                }
+                is Result.Error -> {
+                    userRepository.clearUserData()
+                    null
+                }
+            }
+        }
+    }
+
+    private suspend fun getNewToken(refreshToken: String?): Result<AuthResponse> {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://mooduck-service-api.onrender.com/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .client(okHttpClient)
+            .build()
+        val service = retrofit.create(AuthApi::class.java)
+        return try {
+            Result.Success(
+                withContext(Dispatchers.IO) {
+                    service.refresh(refreshToken!!).await()
+                }
+            )
+        } catch (e: Exception) {
+            Log.d("TAG", e.message.toString())
+            Result.Error(IOException("Error refresh", e))
+        }
+    }
+}
---
+++ Mobile/app/src/main/java/com/example/mts_audio/common/AuthInterceptor.kt
@@ -0,0 +1,19 @@
+package com.example.mts_audio.common
+
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthInterceptor @Inject constructor(
+    private val userRepository: LocalUserRepository
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val token = runBlocking {
+            userRepository.getAccessToken()
+        }
+        val request = chain.request().newBuilder()
+        request.addHeader("Authorization", "Bearer $token")
+        return chain.proceed(request.build())
+    }
+}
