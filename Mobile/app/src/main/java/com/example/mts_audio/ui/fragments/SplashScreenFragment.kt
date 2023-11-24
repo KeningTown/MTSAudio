8fa77396360ffa6037c9362f87fb7696a192ab66
dfoxs353 <platoleg37@gmail.com>
2023-11-24 20:35:18 +0300

    add SplashScreenFragment

---
+++ Mobile/app/src/main/java/com/example/mts_audio/ui/fragments/SplashScreenFragment.kt
@@ -0,0 +1,28 @@
+package com.example.mts_audio.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mts_audio.R
+
+
+
+class SplashScreenFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+
+}
