35aaba2694bfb9a889bb63c23c9488a474bf0c8e
dfoxs353 <platoleg37@gmail.com>
2023-11-24 20:35:18 +0300

    add SplashScreenFragment

---
+++ Mobile/app/src/main/java/com/example/mts_audio/ui/fragments/SplashScrennFragment.kt
@@ -0,0 +1,36 @@
+package com.example.mts_audio.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mts_audio.R
+
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class SplashScrennFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_splash_screnn, container, false)
+    }
+
+
+}
