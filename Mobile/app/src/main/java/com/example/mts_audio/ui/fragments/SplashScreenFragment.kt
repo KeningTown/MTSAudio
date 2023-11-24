e35fd632d6e6c72b9d23716e49b44c563c6d6133
dfoxs353 <platoleg37@gmail.com>
2023-11-24 21:48:03 +0300

    add splashScreen animation and appIcon

--- Mobile/app/src/main/java/com/example/mts_audio/ui/fragments/SplashScreenFragment.kt
+++ Mobile/app/src/main/java/com/example/mts_audio/ui/fragments/SplashScreenFragment.kt
@@ -1,10 +1,12 @@
 package com.example.mts_audio.ui.fragments
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.mts_audio.R
 
 
@@ -14,6 +16,10 @@ class SplashScreenFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        Handler().postDelayed(
+            {
+                findNavController().navigate(R.id.action_splashScreenFragment_to_loginFragment)
+            },4000)
     }
 
     override fun onCreateView(
