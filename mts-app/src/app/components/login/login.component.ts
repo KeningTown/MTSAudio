80a09ac0ee9863cc33800df09fd846540bc42c5b
hotboyinside <rusov_pavel2002@mail.ru>
2023-11-25 21:51:36 +0300

    everything is okey

--- mts-app/src/app/components/login/login.component.ts
+++ mts-app/src/app/components/login/login.component.ts
@@ -1,10 +1,12 @@
-import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms'
+import { AuthService } from '../../services/auth.service';
+import { MessageService } from 'primeng/api';
 
 
 @Component({
@@ -15,8 +17,7 @@ import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms'
     CardModule,
     InputTextModule,
     ReactiveFormsModule,
-    ButtonModule,
-    RouterModule
+    ButtonModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
@@ -27,9 +28,46 @@ export class LoginComponent {
     password: ['', Validators.required]
   });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private msgService: MessageService
+    ) { }
 
   get email() {
     return this.loginForm.controls['email'];
+  }
+
+  get password() {
+    return this.loginForm.controls['password'];
+  }
+
+  private emailErrorShown = false;
+
+  shouldShowEmailError() {
+    const shouldShow = this.email.invalid && (this.email.dirty || this.email.touched) && !this.emailErrorShown;
+    if (shouldShow) {
+      this.emailErrorShown = true;
+    }
+    return shouldShow;
+  }
+
+  loginUser() {
+    const { email, password } = this.loginForm.value;
+    this.authService.getUserByEmail(email as string).subscribe(
+      response => {
+        if (response.length > 0 && response[0].password === password) {
+          sessionStorage.setItem('email', email as string);
+          this.router.navigate(['/home']);
+        } else {
+          this.msgService.add({ severity: 'error', summary: 'Error', detail: 'email or password is wrong' });
+        }
+      },
+      error => {
+        this.msgService.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong' });
+      }
+
+    )
   }
 }
--- mts-app/src/app/interfaces/auth.ts
+++ mts-app/src/app/interfaces/auth.ts
@@ -1,2 +1,5 @@
-export interface Auth {
+export interface User {
+  id: string;
+  email: string;
+  password: string
 }
